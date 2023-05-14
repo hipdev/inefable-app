bc4825504fe46a34bb64c92cfe0ddbbed6bf6fbc
Julian David <julianfullstackdev@gmail.com>
2023-05-14 01:54:39 -0500

    First commit, setup project

--- App.tsx
+++ App.tsx
@@ -1,18 +1,9 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View className='flex-1 items-center justify-center'>
+      <Text className='text-lg'>Aquí estará Inefable :D</Text>
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
